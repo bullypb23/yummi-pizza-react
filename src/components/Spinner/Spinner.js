b5468c504e5d84d2a59c57133d526bd49878f52c
bully23 <bullypb23@gmail.com>
2020-05-29 14:53:43 +0200

    added spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Spinner.module.css';
+
+const Spinner = () => {
+  return (
+    <div className={classes.Spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Spinner;
--- src/containers/Pizzas/Pizzas.js
+++ src/containers/Pizzas/Pizzas.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './Pizzas.module.css';
 import axios from 'axios';
 import Pizza from '../../components/Pizza/Pizza';
+import Spinner from '../../components/Spinner/Spinner';
 
 class Pizzas extends Component {
   state = {
@@ -13,7 +14,7 @@ class Pizzas extends Component {
   componentDidMount() {
     axios.get(this.state.url)
       .then(response => {
-        this.setState({pizzas: response.data})
+        this.setState({pizzas: response.data, isLoaded: true})
       })
   }
 
@@ -23,17 +24,19 @@ class Pizzas extends Component {
 
   render() {
     const pizzas = this.state.pizzas;
-    let items = null;
-    items = Object.keys(pizzas).map(key => {
-      return <Pizza 
-                key={key}
-                name={pizzas[key].name}
-                ingredients={pizzas[key].ingredients}
-                priceS={pizzas[key].priceS}
-                priceL={pizzas[key].priceL}
-                priceXL={pizzas[key].priceXL}
-                orderHandler={this.props.orderHandler} />
-    })
+    let items = <Spinner />;
+    if(this.state.isLoaded) {
+      items = Object.keys(pizzas).map(key => {
+        return <Pizza 
+                  key={key}
+                  name={pizzas[key].name}
+                  ingredients={pizzas[key].ingredients}
+                  priceS={pizzas[key].priceS}
+                  priceL={pizzas[key].priceL}
+                  priceXL={pizzas[key].priceXL}
+                  orderHandler={this.props.orderHandler} />
+      }) 
+    } 
     return (
       <div className={classes.PizzasContainer}>
         {items}
