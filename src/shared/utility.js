dfd578d5fab729cec002439d8e6c700e2fd733d1
bully23 <bullypb23@gmail.com>
2020-06-01 18:15:20 +0200

    changed api url in utility

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -1,3 +1,3 @@
-const API = 'https://cryptic-taiga-42680.herokuapp.com/';
+const API = 'http://cors-anywhere.herokuapp.com/https://cryptic-taiga-42680.herokuapp.com/';
 export const API_PIZZAS = API + 'api/pizzas';
 export const API_STORE_ORDER = API + 'api/orders';
